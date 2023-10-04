e2dfc1ae879ab0caa7268cbd44a3d0dbba1da093
LimeTree0 <bum9516@gmail.com>
2023-10-04 20:22:05 +0900

    깃허브 설정에 맞게 폴더 이름 변경

--- Django_object_detection_server/asgi.py
+++ Django_object_detection_server/asgi.py
@@ -1,4 +1,4 @@
-# mysite/asgi.py
+# Django_object_detection_server/asgi.py
 import os
 
 from channels.auth import AuthMiddlewareStack
@@ -8,7 +8,7 @@ from django.core.asgi import get_asgi_application
 
 from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_object_detection_server.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
